6c9fdc5e392e9a5c7f1e3e556ba4b0ceaa8c8323
Gian20luca <gianluca.bellafronte@gmail.com>
2021-01-14 13:37:46 +0100

    ciao

--- src/App.js
+++ src/App.js
@@ -8,19 +8,8 @@ export function App() {
     <BrowserRouter>
       <div>
         <Route exact path="/" component={HomeComponent}/>
-       {/*  <Route exact path="/ManagePositives" component={ManagePositive}/>
-        <Route exact path="/ManageThresholds" component={ManageThresholds}/>
-        <Route exact path="/Team" component={Team}/> */}
         
       </div>
     </BrowserRouter>
-   /*  <div className="container-fluid">
-      <div className="row">
-        <NavbarComponent />
-        <HomeComponent />
-        <FooterComponent/>
-        
-      </div>
-    </div> */
   )
 }
