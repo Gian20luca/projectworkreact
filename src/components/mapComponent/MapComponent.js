4dbe238034ac99e64745b6f3a964791fdd86058c
Gian20luca <gianluca.bellafronte@gmail.com>
2021-01-16 15:38:12 +0100

    gianluca

--- src/components/mapComponent/MapComponent.js
+++ src/components/mapComponent/MapComponent.js
@@ -197,9 +197,9 @@ export function MapComponent() {
             </ul>
             <Route exact path="/">
             {dbSoglie && <div className='divMaxLegend'>
-                <div className='divLegendSmall'><small>Zona basso rischio {'0% - ' +dbSoglie[0].minThresholds + '%'}</small></div>
-                <div className='divLegendSmall'><small>Zona medio rischio {dbSoglie[0].minThresholds + 1 + '% - ' + dbSoglie[0].maxThresholds + '%'}</small></div>
-                <div className='divLegendSmall'><small>Zona alto rischio {dbSoglie[0].maxThresholds + 1 + '% -   100%' } </small></div>
+                <div className='divLegendSmall'style={{borderColor: dbSoglie[0].minColor}}><small>Zona basso rischio {'0% - ' +dbSoglie[0].minThresholds + '%'}</small></div>
+                <div className='divLegendSmall'style={{borderColor: dbSoglie[0].mediumColor}}><small>Zona medio rischio {dbSoglie[0].minThresholds + 1 + '% - ' + dbSoglie[0].maxThresholds + '%'}</small></div>
+                <div className='divLegendSmall'style={{borderColor: dbSoglie[0].maxColor}}><small>Zona alto rischio {dbSoglie[0].maxThresholds + 1 + '% -   100%' } </small></div>
               </div>}
               {dbSoglie && <div className='divMaxLegend'>
                 <div className='divLegend' style={{backgroundColor: dbSoglie[0].minColor, width: dbSoglie[0].minThresholds + '%' }}></div>
@@ -217,9 +217,9 @@ export function MapComponent() {
             </Route>
             <Route exact path="/mapDeaths">
             {dbSoglie && <div className='divMaxLegend'>
-                <div className='divLegendSmall'><small>Bassa densità decessi {'0% - ' +dbSoglie[1].minThresholds + '%'}</small></div>
-                <div className='divLegendSmall'><small>Media densità decessi {dbSoglie[1].minThresholds + 1 + '% - ' + dbSoglie[1].maxThresholds + '%'}</small></div>
-                <div className='divLegendSmall'><small>Alta densità decessi {dbSoglie[1].maxThresholds + 1 + '% -   100%' } </small></div>
+                <div className='divLegendSmall' style={{borderColor: dbSoglie[1].minColor, }}><small>Bassa densità decessi {'0% - ' +dbSoglie[1].minThresholds + '%'}</small></div>
+                <div className='divLegendSmall' style={{borderColor: dbSoglie[1].mediumColor, }}><small>Media densità decessi {dbSoglie[1].minThresholds + 1 + '% - ' + dbSoglie[1].maxThresholds + '%'}</small></div>
+                <div className='divLegendSmall' style={{borderColor: dbSoglie[1].maxColor,}}><small>Alta densità decessi {dbSoglie[1].maxThresholds + 1 + '% -   100%' } </small></div>
               </div>}
               {dbSoglie && <div className='divMaxLegend'>
                 <div className='divLegend' style={{backgroundColor: dbSoglie[1].minColor, width: dbSoglie[0].minThresholds + '%' }}></div>
@@ -236,9 +236,9 @@ export function MapComponent() {
             </Route>
             <Route exact path="/mapAsymptomatic">
             {dbSoglie && <div className='divMaxLegend'>
-                <div className='divLegendSmall'><small>Zona basso rischio {'0% - ' +dbSoglie[2].minThresholds + '%'}</small></div>
-                <div className='divLegendSmall'><small>Zona medio rischio {dbSoglie[2].minThresholds + 1 + '% - ' + dbSoglie[2].maxThresholds + '%'}</small></div>
-                <div className='divLegendSmall'><small>Zona alto rischio {dbSoglie[2].maxThresholds + 1 + '% -   100%' } </small></div>
+                <div className='divLegendSmall' style={{borderColor: dbSoglie[2].minColor}}><small>Zona basso rischio {'0% - ' +dbSoglie[2].minThresholds + '%'}</small></div>
+                <div className='divLegendSmall' style={{borderColor: dbSoglie[2].mediumColor}}><small>Zona medio rischio {dbSoglie[2].minThresholds + 1 + '% - ' + dbSoglie[2].maxThresholds + '%'}</small></div>
+                <div className='divLegendSmall' style={{borderColor: dbSoglie[2].maxColor}}><small>Zona alto rischio {dbSoglie[2].maxThresholds + 1 + '% -   100%' } </small></div>
               </div>}
               {dbSoglie && <div className='divMaxLegend'>
                 <div className='divLegend' style={{backgroundColor: dbSoglie[2].minColor, width: dbSoglie[0].minThresholds + '%' }}></div>
